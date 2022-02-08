3dd1a548a93dc79df279f1d694e326b3dbc4d1f6
Brian Williams <49660533+bwilliams512@users.noreply.github.com>
2022-02-08 15:36:01 -0600

    Add files via upload

---
+++ PlantRepository.java
@@ -0,0 +1,10 @@
+package com.codecademy.plants.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.codecademy.plants.entities.Plant;
+
+public interface PlantRepository extends CrudRepository<Plant, Integer> {
+}
